b517d53a696da06010a95a69d01f25d106741356
altoid <noelreeds01@gmail.com>
2024-07-31 00:16:16 +0000

    checks for utf-8 str

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""checks for utf-8 character encoding"""
+
+
+def validUTF8(data):
+    if type(data) is not list:
+        return False
+    else
+        for m in data:
+            
