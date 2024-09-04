b723de4b08332ab3fc4e21f6fb8f9b07b4c9ffb5
altoid <noelreeds01@gmail.com>
2024-09-04 23:43:08 +0000

    returns the perimeter of the island

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""returns the perimeter of the island"""
+
+
+def island_perimeter(grid):
+    """function def"""
+    count = [0] * len(grid[0])
+    for row in grid:
+        for index, cols in enumerate(row):
+            if cols != 0:
+                count[index] += 1
+    height = max(count)
+    count2 = []
+    for row in grid:
+        count = 0
+        for cols in row:
+            if cols != 0:
+                count += 1
+        count2.append(count)
+    width = max(count2)
+    perimeter = 2*(height + width)
+    return perimeter
