464d1aa3edc00658d1566bd61ef91c4d243cb72c
altoid <noelreeds01@gmail.com>
2024-09-10 08:34:55 +0000

    sieve of prime

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""prime game module"""
+
+
+def isWinner(nums):
+    """returns name of the player that won the most rounds
+    sieve prime numbers from the nums array."""
+    num = len(nums)
+    prime_nums = [True for _ in range(num+1)]
+    m = 2
+    while m * m <= num:
+        n = m * m
+        while n <= num:
+            prime_nums[n] = False
+            n += m
+        m += 1
+    var = 2
+    while var <= num:
+        if prime_nums[var]:
+            print(var, end=" ")
+        var += 1
