499c28bb41d2643bab7bbf541b585b012238acd8
altoid <noelreeds01@gmail.com>
2024-09-11 11:45:40 +0000

    returns winner

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,11 +2,15 @@
 """prime game module"""
 
 
-def isWinner(nums):
+def sieve_of_eratosthenes(num):
     """returns name of the player that won the most rounds
     sieve prime numbers from the nums array."""
-    num = len(nums)
+    if num < 2:
+        return []
+
     prime_nums = [True for _ in range(num+1)]
+    prime_nums[0] = prime_nums[1] = False
+
     m = 2
     while m * m <= num:
         n = m * m
@@ -14,10 +18,38 @@ def isWinner(nums):
             prime_nums[n] = False
             n += m
         m += 1
-    var = 2
-    prime = []
-    while var <= num:
-        if prime_nums[var]:
-            prime.append(var)
-        var += 1
+    prime = [x for x in range(2, num + 1) if prime_nums[x]]
     return prime
+
+
+def game(n, prime_nums):
+    prime_numbers = [num for num in range(n + 1) if prime_nums[num]]
+    turn = 0
+
+    while prime_numbers:
+        chosen = prime_numbers.pop(0)
+        print(f"Chosen prime: {chosen}")
+        prime_numbers = [m for m in prime_numbers if m % chosen != 0]
+        turn = 1 - turn
+
+    return "Ben" if turn == 0 else "Maria"
+
+
+def isWinner(x, nums):
+    num = max(nums)
+    prime_nums = sieve_of_eratosthenes(num)
+
+    Maria = 0
+    Ben = 0
+    for m in nums:
+        winner = game(m, prime_nums)
+        if winner == "Maria":
+            Maria += 1
+        else:
+            Ben += 1
+    if Maria > Ben:
+        return "Maria"
+    elif Ben > Maria:
+        return "Ben"
+    else:
+        return None
