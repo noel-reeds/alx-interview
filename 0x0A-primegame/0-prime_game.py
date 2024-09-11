571b01f4b83f23a08442146e07c7d425c85da874
altoid <noelreeds01@gmail.com>
2024-09-11 11:52:12 +0000

    pycodestyle validated

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -5,13 +5,13 @@
 def sieve_of_eratosthenes(max_n):
     is_prime = [True] * (max_n + 1)
     is_prime[0] = is_prime[1] = False
-    
     for i in range(2, int(max_n ** 0.5) + 1):
         if is_prime[i]:
             for j in range(i * i, max_n + 1, i):
                 is_prime[j] = False
-                
+
     return is_prime
+
 
 def game(n, prime_nums):
     prime_numbers = [num for num in range(n + 1) if prime_nums[num]]
