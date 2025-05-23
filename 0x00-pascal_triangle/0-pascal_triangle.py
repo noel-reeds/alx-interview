052ba17de7d415cd71a47ce6000ca42f23e94d64
altoids <noelreeds01@gmail.com>
2025-05-23 17:27:02 +0300

    returns a list of lists from Pascal's Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Returns a list of lists of integers"""
+
+def pascal_triangle(n):
+    # n represents number of rows in a triangle
+    if n <= 0:
+        return []
+    else:
+        # iterate through the rows
+        for num in range(n):
+            # number of elements in each row is n+1
+            pass
