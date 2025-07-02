bd3ffae6faebcaa4b528ce754ec4b73155a11acf
altoids <noelreeds01@gmail.com>
2025-07-02 10:12:41 +0300

    determines if all the boxes can be opened.

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Lockboxes"""
+
+
+def canUnlockAll(boxes):
+    """
+    method that determines if all the boxes can be opened
+    boxes argument is a list of lists
+    """
+
