e39eb79af8a20f968d4ece7b48df624da9e31bb5
altoids <noelreeds01@gmail.com>
2025-07-03 11:32:30 +0300

    checks if already opened box has keys

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,6 +5,9 @@
 def canUnlockAll(boxes):
     """
     method that determines if all the boxes can be opened
-    boxes argument is a list of lists
+    Args: boxes argument is a list of lists
     """
-
+    # check if the first box has keys
+    if len(boxes[0]) < 1:
+        return False
+    # otherwise retrieve keys and open the rest of the boxes
