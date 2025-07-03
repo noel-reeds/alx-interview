849190b40b4b3a43344829b5f111c7d872345de3
altoids <noelreeds01@gmail.com>
2025-07-03 17:58:23 +0300

    appends keys in opened boxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -10,4 +10,8 @@ def canUnlockAll(boxes):
     # check if the first box has keys
     if len(boxes[0]) < 1:
         return False
-    # otherwise retrieve keys and open the rest of the boxes
+    # otherwise retrieve keys
+    open_boxes = boxes[0]
+    for box in boxes[1:]:
+        if box in open_boxes:
+            open_boxes.append(key for key in box and key not in open_boxes)
